1b16ae82727bcbb415ddb6a622033b21c3c243ba
talexandru11 <tanasealexandru11@gmail.com>
2025-10-23 16:15:10 +0300

    Initial commit

---
+++ chess.cpp
@@ -0,0 +1,17 @@
+#define _SECURE_CRT_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+class PiesaSah {
+public:
+	string nume;
+	bool culoare;
+	bool activa;
+};
+
+
+
+void main() {
+	return 0;
+}
