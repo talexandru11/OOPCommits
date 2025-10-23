c00c7a1af53b8346a27456192e9ce1852f28e0ca
Alexandru Tanase <31417726+talexandru11@users.noreply.github.com>
2025-10-23 14:52:07 +0300

    First commit
    Added the first .cpp file.

---
+++ Chess.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class PiesaSah {
+public:
+	string numePiesa;
+	bool activ; //exista sau nu pe tabla
+	bool culoare; //1 = alb, 0 = negru
+
+	void mutare() {
+	
+	}
+};
+
+void main() {
+	class PiesaSah();
