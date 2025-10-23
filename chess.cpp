c895bc7fe0b04685dab7ae15d0ff49c6b5dc79f3
talexandru11 <tanasealexandru11@gmail.com>
2025-10-23 16:20:42 +0300

    commit test

--- chess.cpp
+++ chess.cpp
@@ -5,9 +5,13 @@ using namespace std;
 
 class PiesaSah {
 public:
-	string nume;
-	bool culoare;
-	bool activa;
+	string nume; 
+	bool culoare; // 1=alb, 0=negru
+	bool activa; // 1=exista, 0=eliminata
+
+	void mutare() {
+		cout << "Piesa se muta pe tabla de sah." << endl;
+	}
 };
 
 
